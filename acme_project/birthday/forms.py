666d506ddce4d5a76e661a3cea67619b8fa45dfc
Дмитрий Майоров <dimayorov@yandex.ru>
2025-08-16 08:50:03 +0300

    First commit

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,54 @@
+# birthday/forms.py
+from django import forms
+from django.core.exceptions import ValidationError
+# Импортируем класс модели Birthday.
+from .models import Birthday
+from .validators import real_age
+
+# Импорт функции для отправки почты.
+from django.core.mail import send_mail
+
+# Для использования формы с моделями меняем класс на forms.ModelForm.
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
+
+
+class BirthdayForm(forms.ModelForm):
+    # Удаляем все описания полей.
+    birthday = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'type': 'date'},
+            format='%Y-%m-%d'
+        ),
+        validators=[real_age]
+    )
+    # Все настройки задаём в подклассе Meta.
+
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        return first_name.split()[0]
+
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+# Импортируется функция-валидатор.
+from .validators import real_age
+# Импортируем функцию reverse() для получения ссылки на объект.
+from django.urls import reverse
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+    )
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+    constraints = (models.UniqueConstraint(
+        # имя ограничения
+        fields=('first_name', 'last_name', 'birthday'),
+        name='Unique person constraint',),)
+
+    class Meta:
+        pass
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
